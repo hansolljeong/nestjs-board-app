cf64ee395dc90d8e75bf9683e00b4b747819c7c5
Hansol <losnah24@gmail.com>
2023-05-27 14:45:54 +0900

    board 생성 API

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
   private boards: Board[] = [];
+
   getAllBoards = () => {
     return this.boards;
   };
+
+  createBoard(title: string, description: string): Board {
+    const board: Board = {
+      id: uuid,
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+    this.boards.push(board);
+    return board;
+  }
 }
