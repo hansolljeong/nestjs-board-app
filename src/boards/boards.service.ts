78a5d8454570f40cddd151ad9bda8dfa960263f2
Hansol <losnah24@gmail.com>
2023-05-29 19:42:44 +0900

    특정 게시물을 찾을 때 없는 경우 결과 값 및 에러 처리

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -25,7 +25,12 @@ export class BoardsService {
   }
 
   getBoardById(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const found = this.boards.find((board) => board.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Can't find board with id ${id}`);
+    }
+    return found;
   }
 
   deleteBoardById(id: string): void {
