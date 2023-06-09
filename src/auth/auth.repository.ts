066f7e5b0d04a4c175b69940a40495370fd3ba59
Hansol <losnah24@gmail.com>
2023-06-09 18:00:32 +0900

    username 중복 시 에러 처리

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
@@ -11,9 +15,17 @@ export class UserRepository {
     this.#userRepository = this.dataSource.getRepository(UserEntity);
   }
 
-  createUser(authCredentialDto: AuthCredentialDto): Promise<UserEntity> {
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<UserEntity> {
     const user = this.#userRepository.create(authCredentialDto);
 
-    return this.#userRepository.save(user);
+    try {
+      return await this.#userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Exsisting username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,13 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity('user')
+@Unique(['username'])
 export class UserEntity extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
